33ada5d317fda304cbf84e9a5524db1a1fa7632a
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-03 20:14:25 -0700

    Created 4 routes for 4 components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import StreamDelete from './Components/StreamDelete'
+import StreamCreate from './Components/StreamCreate'
+import StreamEdit from './Components/StreamEdit'
+import StreamList from './Components/StreamList'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
+const App = () => {
+  return <div>
+    <BrowserRouter>
+      <div>
+        <Route to="/" exact component={StreamList} />
+        <Route to="/StreamEdit" exact component={StreamEdit} />
+        <Route to="/StreamCreate" exact component={StreamCreate} />
+        <Route to="/StreamDelete" exact component={StreamDelete} />
       </div>
-    );
-  }
+
+    </BrowserRouter>
+    </div>
+
+  
 }
 
-export default App;
+export default App
